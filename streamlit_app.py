0f9075b0b11ba31b3440070e05fc931fd8606936
Narayanswamy Venkiteswaran <105338689+venkitn@users.noreply.github.com>
2022-05-11 16:46:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ streamlit.text('Hard Boiled Free-Range Egg')
 streamlit.header ('Build Your Own Fruit Smoothie')
 
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
+my_fruit_list = my_fruit_list.index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
